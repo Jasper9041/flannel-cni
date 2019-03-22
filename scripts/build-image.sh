b4e521407ad7cf4bccd9f53d27c7bbfbf6f18b23
Jasper Beaurain <info@jasperbeauraain.com>
2019-03-22 03:47:03 +0100

    customize docker image

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,7 +2,7 @@
 set -e
 CURL="curl -sSf"
 FLANNEL_CNI_ROOT=$(git rev-parse --show-toplevel)
-IMAGE_NAME=quay.io/coreos/flannel-cni
+IMAGE_NAME=jasper9041/flannel-cni
 VERSION=$($FLANNEL_CNI_ROOT/scripts/git-version)
 CNI_VERSION="v0.6.0"
 ARCH=$(uname -m)
